0a04ffa5a31912bd666f109b72d5350c64fb4182
thuannvc <nguyen.van.thuan-c@sun-asterisk.com>
2022-03-25 22:36:10 +0700

    Initial Commit

---
+++ fitness/fitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitnessApp.swift
+//  fitness
+//
+//  Created by nguyen.van.thuanc on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct fitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
